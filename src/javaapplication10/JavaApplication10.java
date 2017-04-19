3c15583bbe6f48b034866e5d9266491b6b5f06e8
Invitado <Invitado@PC05>
2017-04-19 11:04:09 -0500

    operacion

--- src/javaapplication10/JavaApplication10.java
+++ src/javaapplication10/JavaApplication10.java
@@ -15,7 +15,9 @@ public class JavaApplication10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here 
+        System.out.println("hghghgh");
+       
     }
     
 }
